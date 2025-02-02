9401ebd6a3248d357c6b753e05e547b5951e2692
K3irill <razdor.2019@mail.ru>
2025-02-02 18:17:49 +0300

    faet: swiper

---
+++ src/components/Swiper/content.ts
@@ -0,0 +1,5 @@
+export const bannerSlides = [
+	{ id: 1, src: '/img/slides/banner1.png', alt: '1' },
+	{ id: 2, src: '/img/slides/banner2.png', alt: '2' },
+	{ id: 3, src: '/img/slides/banner3.png', alt: '3' },
+]
